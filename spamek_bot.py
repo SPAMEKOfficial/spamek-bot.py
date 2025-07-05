a70188fd517bd99f4761b4d67da19030aca8a2e7
SPAMEK <kara420flex@gmail.com>
2025-07-05 10:39:24 +0200

    Update spamek_bot.py

--- spamek_bot.py
+++ spamek_bot.py
@@ -196,7 +196,3 @@ if __name__ == '__main__':
     threading.Thread(target=run_bot).start()
     app.run(host="0.0.0.0", port=10000)
 
-
-if __name__ == '__main__':
-    threading.Thread(target=run_bot).start()
-    app.run(host="0.0.0.0", port=10000)
